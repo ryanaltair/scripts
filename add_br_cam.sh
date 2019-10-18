7fd476be7d9c1080ecbfcab9414b4b55125bcd4c
Ryanaltair <al_tair@live.com>
2019-10-18 17:38:46 +0800

    Update add_br_cam.sh

--- add_br_cam.sh
+++ add_br_cam.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# chkconfig: 2345 10 90
+# chkconfig: 2345 90 90
 # description: add networks to brcam
 # edit NET_NAMES to your network name
-NET_NAMES=(enp6s0 enp7s0 enp8s0)
+NET_NAMES=(enp1s0f0 enp1s0f1 enp1s0f2 enp1s0f3 enp3s0f0 enp3s0f3)
 brctl addbr brcam
 for NET_NAME in ${NET_NAMES[@]}
-do  
+do
     echo adding ${NET_NAME} to brcam net_bridge
     brctl addif brcam ${NET_NAME}
     echo setting ${NET_NAME} mtu to 9000
