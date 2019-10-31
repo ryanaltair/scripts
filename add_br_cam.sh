45280454e8678c5cc1da4005ae8ad30f05d2f447
Ryanaltair <al_tair@live.com>
2019-10-31 14:43:51 +0800

    Update add_br_cam.sh

--- add_br_cam.sh
+++ add_br_cam.sh
@@ -3,6 +3,10 @@
 # description: add networks to brcam
 # edit NET_NAMES to your network name
 NET_NAMES=(enp1s0f0 enp1s0f1 enp1s0f2 enp1s0f3 enp3s0f0 enp3s0f3)
+# notice: ip should never conflict
+
+# sometimes you need to set net ip address
+# ifconfig enp1s0f0 192.168.10.21 
 brctl addbr brcam
 for NET_NAME in ${NET_NAMES[@]}
 do
@@ -11,5 +15,8 @@ do
     echo setting ${NET_NAME} mtu to 9000
     ifconfig ${NET_NAME} mtu 9000
 done
+# provide brcam ip address
 ifconfig brcam 192.168.10.1
+ 
+
 echo use brctl show brcam to check
